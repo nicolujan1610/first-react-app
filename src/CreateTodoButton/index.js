893b6ff2122914ebe5ac01d02558b19d527e29e2
Nicolas Lujan <nico512lujan@gmail.com>
2022-11-08 16:43:06 -0300

    Curso casi terminado, resta el deploy a GH Pages

---
+++ src/CreateTodoButton/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './CreateTodoButton.css';
+
+function CreateTodoButton(props) {
+  const onClickButton = ()=>{
+    props.setOpenModal(state => !state);
+  }
+  
+  return (
+    <button 
+      className="CreateTodoButton"
+      onClick={onClickButton}
+    >
+      +
+    </button>
+  );
+}
+
+export { CreateTodoButton };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App/index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,5 +9,4 @@ root.render(
     <App /> 
   </React.StrictMode>
 );
-
 
